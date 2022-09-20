4f99e9cba5449276838fec5b93863bbbbda800b5
maximskorik <maximskoryk@gmail.com>
2022-09-20 15:01:48 +0200

    add msp->mgf converter; add data to .gitignore

---
+++ benchmarking/msp_to_mgf.py
@@ -0,0 +1,13 @@
+import sys
+
+from matchms.importing import load_from_msp
+from matchms.exporting import save_as_mgf
+
+
+def msp_to_mgf(msp_file: str, mgf_file: str):
+    spectra = list(load_from_msp(msp_file, metadata_harmonization=False))
+    save_as_mgf(spectra, mgf_file)
+
+
+if __name__ == "__main__":
+    msp_to_mgf(sys.argv[2], sys.argv[3])
