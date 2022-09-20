13a6de1def2e02183d912f687fb5b2d205602c68
maximskorik <maximskoryk@gmail.com>
2022-09-20 17:01:31 +0200

    finish profiler

--- benchmarking/profiling.py
+++ benchmarking/profiling.py
@@ -1,16 +1,14 @@
 import cProfile
+import itertools
 from pathlib import Path
+import pstats
+import os
 
 from matchms.importing import load_from_mgf, load_from_msp
 from matchms.logging_functions import reset_matchms_logger, set_matchms_logger_level
 
-LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 
-
-def read_data(file: str, loglevel: str = 'INFO', harmonization: bool = True):
-    reset_matchms_logger()
-    set_matchms_logger_level(loglevel)
-
+def read_data(file: str, harmonization: bool = True):
     if file.endswith(".msp"):
         spectra = list(load_from_msp(file, metadata_harmonization=harmonization))
     elif file.endswith(".mgf"):
@@ -21,5 +19,25 @@ def read_data(file: str, loglevel: str = 'INFO', harmonization: bool = True):
     return spectra
 
 
+def main(filename, loglevel, harmonization):
+    reset_matchms_logger()
+    set_matchms_logger_level(loglevel)
+    read_data(filename, harmonization=harmonization)
+
+
 if __name__ == "__main__":
-    cProfile.run('read_data("NIST_EI_MS.msp", harmonization=False)')
+    filenames = ["GNPS-LIBRARY.mgf", "GNPS-LIBRARY.msp", "NIST_EI_MS.mgf", "NIST_EI_MS.msp"]
+    loglevels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    harmonizations = [True, False]
+
+    grid = itertools.product(filenames, loglevels, harmonizations)
+    for filename, loglevel, harmonization in grid:
+        print(f"Profiling {filename} with loglevel {loglevel} and harmonization {harmonization}...")
+        profiler = cProfile.Profile()
+        profiler.enable()
+        main(filename=filename, loglevel=loglevel, harmonization=harmonization)
+        profiler.disable()
+
+        pstats.Stats(profiler)\
+            .strip_dirs()\
+            .dump_stats(os.path.join("profiles", f"{Path(filename).stem}_{loglevel}_{harmonization}.prof"))
