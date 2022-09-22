29ac818092071ce585d8f098b2fabe4c61ff99c5
maximskorik <maximskoryk@gmail.com>
2022-09-22 12:24:40 +0200

    add csv profiles, notebook, and utils.py

--- benchmarking/utils.py
+++ benchmarking/utils.py
@@ -1,5 +1,7 @@
 import cProfile
 import io
+import pandas as pd
+from pathlib import Path
 import pstats
 
 
@@ -12,3 +14,25 @@ def prof_to_csv(prof: cProfile.Profile):
     result = 'ncalls' + result.split('ncalls')[-1]
     lines = [','.join(line.rstrip().split(None, 5)) for line in result.split('\n')]
     return '\n'.join(lines)
+
+
+class Profile:
+    def __init__(self, profile_path):
+        self.profile_path = profile_path
+        self.profile_name = Path(profile_path).stem
+        self.profile = pd.read_csv(profile_path)
+        self.library, self.fileformat, self.loglevel, self.harmonization = self.parse_profile_metadata()
+
+    def parse_profile_metadata(self):
+        if self.profile_name.startswith("NIST"):
+            library = "NIST"
+        else:
+            library = "GNPS"
+
+        *_, fileformat, loglevel, harmonization = self.profile_name.split("_")
+        fileformat = fileformat.lstrip(".")
+
+        return library, fileformat, loglevel, harmonization
+
+    def __repr__(self):
+        return self.profile_name
