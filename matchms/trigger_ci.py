8862e93f062ca31dc3df09c88d1bd155519ba1bb
maximskorik <maximskoryk@gmail.com>
2022-12-09 21:26:12 +0100

    add code to trigger Sonar scan

---
+++ matchms/trigger_ci.py
@@ -0,0 +1,20 @@
+def trigger_ci():
+    """
+    add at least 20 lines to generate sonarcloud report
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam quis quam. Proin pede metus, vulputate nec,
+    fermentum fringilla, vehicula vitae, justo. Pellentesque arcu. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero,
+    eget facilisis enim ipsum id lacus. Vivamus ac leo pretium faucibus. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis,
+    sem. Maecenas libero. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Duis ante orci, molestie vitae vehicula venenatis,
+    tincidunt ac pede. In dapibus augue non sapien. Morbi scelerisque luctus velit. Praesent in mauris eu tortor porttitor accumsan. Nullam feugiat,
+    turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Suspendisse nisl. Proin mattis lacinia justo.
+    Nulla pulvinar eleifend sem. Praesent vitae arcu tempor neque lacinia pretium.
+    Curabitur bibendum justo non orci. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Mauris elementum mauris vitae tortor.
+    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vel lectus. Donec odio tempus molestie,
+    porttitor ut, iaculis quis, sem. Nunc tincidunt ante vitae massa. Nulla quis diam. Etiam dui sem, fermentum vitae, sagittis id, malesuada
+    in, quam. Phasellus et lorem id felis nonummy placerat. Maecenas aliquet accumsan leo. Nunc tincidunt ante vitae massa. Duis sapien nunc,
+    commodo et, interdum suscipit, sollicitudin et, dolor. Integer pellentesque quam vel velit. Etiam egestas wisi a erat. Vivamus ac leo pretium
+    faucibus. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Mauris elementum mauris
+    vitae tortor. Et harum quidem rerum facilis est et expedita distinctio. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
+    saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+    """
+    print('triggered ci')
